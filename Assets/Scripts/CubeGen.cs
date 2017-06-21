d1298ca4cd90557e12e148e212d3681e7fb147cf
t-zouchi <mojamoja.000@gmail.com>
2017-06-21 20:22:25 +0900

    add generate method

---
+++ Assets/Scripts/CubeGen.cs
@@ -0,0 +1,29 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class CubeGen : MonoBehaviour {
+  public GameObject cube;
+	// Use this for initialization
+	void Start () {
+		
+	}
+	
+	// Update is called once per frame
+	void Update () {
+    Ray ray;
+    RaycastHit hit;
+
+    if(Input.GetMouseButtonDown(0))
+    {
+      ray = Camera.main.ScreenPointToRay(Input.mousePosition);
+
+      if(Physics.Raycast(ray, out hit, 100))
+      {
+        Debug.Log("x: " + Input.mousePosition.x + " y: " + Input.mousePosition.y + " z: "  + Input.mousePosition.z);
+        Instantiate(cube, transform.position, transform.rotation);
+
+      }
+    }
+	}
+}
