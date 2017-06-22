f2963824aa73dc4f9e3618e84ffd1895b4e6d9e1
t-zouchi <mojamoja.000@gmail.com>
2017-06-22 15:55:28 +0900

    update drow logic

--- Assets/Scripts/CubeGen.cs
+++ Assets/Scripts/CubeGen.cs
@@ -4,9 +4,10 @@ using UnityEngine;
 
 public class CubeGen : MonoBehaviour {
   public GameObject cube;
-	// Use this for initialization
-	void Start () {
-		
+  float drewTime;
+  // Use this for initialization
+  void Start () {
+    drewTime = Time.deltaTime;
 	}
 	
 	// Update is called once per frame
@@ -14,24 +15,32 @@ public class CubeGen : MonoBehaviour {
     Ray ray;
     RaycastHit hit;
     Vector3 hitPoint;
+    
 
-    if(Input.GetMouseButton(0))
+    if (Input.GetMouseButton(0))
     {
       ray = Camera.main.ScreenPointToRay(Input.mousePosition);
 
-      if(Physics.Raycast(ray, out hit, 100))
+      if (Physics.Raycast(ray, out hit, 100))
       {
+        hitPoint = hit.point;
+        if (hitPoint.y < 0.05f)
+        {
+          hitPoint.y = 0.05f;
+        }
+
         if (hit.collider.tag == "Plane")
         {
-          hitPoint = hit.point;
-          if (hitPoint.y < 0.05f)
-          {
-            hitPoint.y = 0.05f;
-          }
           Instantiate(cube, hitPoint, transform.rotation);
         }
         else if (hit.collider.tag == "cube")
         {
+          if (drewTime > 0.075)
+          {
+            Instantiate(cube, hitPoint, transform.rotation);
+            drewTime = 0;
+          }
+          drewTime += Time.deltaTime;
         }
       }
     }
