fcf0cc7c558d8255835028ad05c8e0205ce00661
t-zouchi <mojamoja.000@gmail.com>
2017-06-23 22:02:02 +0900

    set color changer

--- Assets/Scripts/CubeGen.cs
+++ Assets/Scripts/CubeGen.cs
@@ -12,7 +12,7 @@ public class CubeGen : MonoBehaviour {
   void Start () {
     drewTime = Time.deltaTime;
     color = GetComponent<colorManager>();
-    cube = color.set_000000();
+    set_000000();
 	}
 	
 	// Update is called once per frame
@@ -67,5 +67,84 @@ public class CubeGen : MonoBehaviour {
   public void setEreaseMode()
   {
     mode = 1;
+  }
+
+  public void set_000000()
+  {
+    cube = color.get_000000();
+  }
+
+  public void set_0000FF()
+  {
+    cube = color.get_0000FF();
+  }
+
+  public void set_00FF00()
+  {
+    cube = color.get_00FF00();
+  }
+
+  public void set_00FFFF()
+  {
+    cube = color.get_00FFFF();
+  }
+
+  public void set_80FF80()
+  {
+    cube = color.get_80FF80();
+  }
+
+  public void set_80FFFF()
+  {
+    cube = color.get_80FFFF();
+  }
+
+  public void set_8080FF()
+  {
+    cube = color.get_8080FF();
+  }
+
+  public void set_555555()
+  {
+    cube = color.get_555555();
+  }
+
+  public void set_CCCCCC()
+  {
+    cube = color.get_CCCCCC();
+  }
+
+  public void set_FF0000()
+  {
+    cube = color.get_FF0000();
+  }
+  public void set_FF8080()
+  {
+    cube = color.get_FF8080();
+  }
+
+  public void set_FF00FF()
+  {
+    cube = color.get_FF00FF();
+  }
+
+  public void set_FF80FF()
+  {
+    cube = color.get_FF80FF();
+  }
+
+  public void set_FFFF00()
+  {
+    cube = color.get_FFFF00();
+  }
+
+  public void set_FFFF80()
+  {
+    cube = color.get_FFFF80();
+  }
+
+  public void set_FFFFFF()
+  {
+    cube = color.get_FFFFFF();
   }
 }
--- Assets/Scripts/colorManager.cs
+++ Assets/Scripts/colorManager.cs
@@ -21,8 +21,83 @@ public class colorManager : MonoBehaviour {
   public GameObject _FFFF80;
   public GameObject _FFFFFF;
 
-  public GameObject set_000000()
+  public GameObject get_000000()
   {
     return _000000;
+  }
+
+  public GameObject get_0000FF()
+  {
+    return _0000FF;
+  }
+
+  public GameObject get_00FF00()
+  {
+    return _00FF00;
+  }
+
+  public GameObject get_00FFFF()
+  {
+    return _00FFFF;
+  }
+
+  public GameObject get_80FF80()
+  {
+    return _80FF80;
+  }
+
+  public GameObject get_80FFFF()
+  {
+    return _80FFFF;
+  }
+
+  public GameObject get_8080FF()
+  {
+    return _8080FF;
+  }
+
+  public GameObject get_555555()
+  {
+    return _555555;
+  }
+
+  public GameObject get_CCCCCC()
+  {
+    return _CCCCCC;
+  }
+
+  public GameObject get_FF0000()
+  {
+    return _FF0000;
+  }
+
+  public GameObject get_FF00FF()
+  {
+    return _FF00FF;
+  }
+
+  public GameObject get_FF80FF()
+  {
+    return _FF80FF;
+  }
+
+  public GameObject get_FF8080()
+  {
+    return _FF8080;
+  }
+
+  public GameObject get_FFFF00()
+  {
+    return _FFFF00;
+  }
+
+  public GameObject get_FFFF80()
+  {
+    return _FFFF80;
+  }
+
+  public GameObject get_FFFFFF()
+  {
+    return _FFFFFF;
   }
 }
