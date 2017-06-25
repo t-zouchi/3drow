ca63d14227d3e5a8aecc1fe077a7f926dec41bcd
t-zouchi <mojamoja.000@gmail.com>
2017-06-25 13:24:25 +0900

    add how to

---
+++ Assets/Scripts/TextController.cs
@@ -0,0 +1,69 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+using UnityEngine.UI;
+
+public class TextController : MonoBehaviour {
+  string str;
+  Text text;
+  int counter = 0;
+	// Use this for initialization
+	void Start () {
+    str = getText1();
+    text = GetComponent<Text>();
+    text.text = str;
+	}
+	 
+  void changeStr()
+  {
+    Debug.Log(counter);
+    switch (counter)
+    {
+      case 0:
+        str = getText1();
+        break;
+      case 1:
+        str = getText2();
+        break;
+      case 2:
+        str = getText3();
+        break;
+      default:
+        break;
+    }
+    text.text = str;
+  }
+
+  public string getText1()
+  {
+    return "操作\n・左クリック\n床またはcubeをクリックして、cubeを生成\n操作パネルで生成、消しゴムモード切り替え、cubeの色を選択";
+  }
+
+  public string getText2()
+  {
+    return "カメラ操作\nカメラはUnityのSceneビューと同じ用に操作することができます\n\n・右クリックドラッグ操作\n右クリックしながら操作することで、カメラの向きを変えることができます。\n\n・マウスホイール操作\nマウスホイールを操作することで、画面の拡大、縮小ができます。\n\n・マウスホイールドラッグ操作\nマウスホイールをクリックしたままドラッグすることで、カメラを上下左右に平行移動することができます\n";
+  }
+
+  public string getText3()
+  {
+    return "補足\n作成した3Dオブジェクトを保存する機能は実装されていません\n\nゲーム終了前にスクリーンショットの保存等をおすすめします。\n保存したスクリーンショットは#3drowを付けてtweetして頂けると、開発者が喜びます。";
+  }
+
+  public void OnLeft()
+  {
+    if(counter < 2)
+    {
+      counter++;
+    }
+    changeStr();
+  }
+
+  public void OnRight()
+  {
+    if (counter > 0)
+    {
+      counter--;
+    }
+    changeStr();
+  }
+}
